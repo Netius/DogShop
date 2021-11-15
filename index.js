4285f0ce2b1710977e63417837acdfe0e8c133a1
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2021-11-15 11:30:41 +0100

    Added Hero component

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-import   navbar   from "./js/components/navbar.js";
+import navbar from "./js/components/navbar.js";
+import hero from "./js/components/hero.js";
 
 // Navigation bar
 navbar();
+// Hero on the homepage
+hero();
