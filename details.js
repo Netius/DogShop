03da0a5f23a2341eaa3994fe90d334237df91760
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2021-12-02 19:52:38 +0100

    Added details to prj

---
+++ details.js
@@ -0,0 +1,8 @@
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import navbar from "./js/components/navbar.js";
+import footer from "./js/components/footer.js";
+
+navbar();
+
+footer();
+
