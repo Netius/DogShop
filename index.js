c8483831a906fccb372d6f306427eb9160fd666d
NetiusSP <fabiomoraisnetosp@hotmail.com>
2021-12-02 13:56:44 +0100

    Moved hero from js to html

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import navbar from "./js/components/navbar.js";
-import hero from "./js/components/hero.js";
+import { fetchHeroImage } from "./js/components/hero.js";
 import footer from "./js/components/footer.js";
 import { fetchProducts } from "./js/components/cards.js";
 
-// Navigation bar
 navbar();
-// Hero on the homepage
-hero();
+fetchHeroImage();
 fetchProducts();
 footer();
 
