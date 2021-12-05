e8e29975fdc3d7e1f7e563b66565a80be4bf5f1d
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2021-12-05 12:41:44 +0100

    Deleted js navbar.js and inserted in html

--- details.js
+++ details.js
@@ -1,14 +1,9 @@
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-import navbar from "./js/components/navbar.js";
 import footer from "./js/components/footer.js";
 import { productStrapiUrl } from "./js/constants/strapiUrl.js";
 import { fetchStrapi } from "./js/utils/fetchStrapi.js";
 import getUrlParamsId from "./js/utils/getUrlParamsId.js";
 import productDetails from "./js/components/details/productDetails.js";
-
-
-
-navbar();
 
 const productId= getUrlParamsId();
 
--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-import navbar from "./js/components/navbar.js";
 import { heroImage } from "./js/components/hero.js";
 import footer from "./js/components/footer.js";
 import cardsProducts from "./js/components/cards.js";
 import { heroStrapiUrl, productStrapiUrl } from "./js/constants/strapiUrl.js";
 import { fetchStrapi } from "./js/utils/fetchStrapi.js";
 
-
-navbar();
 // Fetch hero image from strapi
 fetchStrapi(heroStrapiUrl, "#hero__spinner")
     .then(hero => {
