46eede21d66b76869c1ec5059996d39cf92e3949
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2021-12-05 12:36:44 +0100

    Changed price to tag p

--- js/components/details/productDetails.js
+++ js/components/details/productDetails.js
@@ -11,19 +11,19 @@ export default function productDetails(product){
                     <div class="col-lg-6 col-sm-12">
                         <h2 class="display-6 mt-3">${product.title}</h2>
                         <p>${product.description}</p>
-                        <h3 class="mt-5 mb-3">$${product.price}</h3>
-                        <button class="zoom-button btn btn-dark btn-rounded mr-1" data-toggle="tooltip" title="Add to cart" data-original-title="Add to cart">
+                        <p class="mt-5 mb-3 h3">$${product.price}</p>
+                        <button class="zoom-button btn btn-primary btn-rounded mr-1" data-toggle="tooltip" title="Add to cart" data-original-title="Add to cart">
                             <i class="fa fa-shopping-cart"></i>
                         </button>
                         <button class="ms-1 zoom-button fs-6 btn btn-feature" 
                                     title="Buy now" href="products.html" role="button">
                                     Buy now
                         </button>
-                        <h3 class="box-title mt-5">Key Highlights</h3>
+                        <h3 class="mt-5">Features</h3>
                         <ul class="list-unstyled">
-                            <li><i class="fa fa-check text-success"></i>Sturdy structure</li>
-                            <li><i class="fa fa-check text-success"></i>Designed to foster easy portability</li>
-                            <li><i class="fa fa-check text-success"></i>Perfect furniture to flaunt your wonderful collectibles</li>
+                            <li><i class="fa fa-check text-success"></i> Made of 100% recicle material</li>
+                            <li><i class="fa fa-check text-success"></i> Easy to wash and clean</li>
+                            <li><i class="fa fa-check text-success"></i> Perfect for your dog</li>
                         </ul>
                     </div>
                 </div>
