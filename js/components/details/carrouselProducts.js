ee3a27ba6bfcd15242f19b70a682bdf23e1b8536
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2021-12-06 22:43:42 +0100

    Better logic for carrousel slider

--- js/components/details/carrouselProducts.js
+++ js/components/details/carrouselProducts.js
@@ -1,16 +1,13 @@
 import { strapiUrl } from "../../constants/strapiUrl.js";
 
 export default function carrouselProducts(products){
-    const carrouselFirst = document.getElementById("carrousel-first");
-    const carrouselSecond = document.getElementById("carrousel-second");
+    const carrouselContainer = document.getElementById("carrousel-container");
   
     let counter = 0;
+    let firstSlider, secondSlider, thirdSlider, allSlider = "";
 
-    products.forEach(product => {
-
-    console.log(counter)
-    if(counter < 3){    
-    carrouselFirst.innerHTML += `
+    products.forEach(product => {  
+    allSlider += `
         <div class="col-lg-4 col-md-6 mb-4">
             <a href="details.html?id=${product.id}" title="Shop ${product.title}">    
                 <div class="card rounded shadow-lg border-0 h-100 zoom-button">
@@ -26,33 +23,39 @@ export default function carrouselProducts(products){
             </a>    
         </div>
         `
+    if(counter === 2){
+        firstSlider = `
+        <div class="carousel-item active">
+            <div class="row">${allSlider}</div>
+        </div>
+        `; 
+        allSlider = "";     
     }
-
+    if(counter === 5){
+        secondSlider = `
+        <div class="carousel-item">
+            <div class="row">${allSlider}</div>
+        </div>
+        `; 
+        allSlider = "";     
+    }
     
-    
-    // if(counter >= 3 && counter < 6){
-    // carrouselSecond.innerHTML += `
-    //     <div class="col-lg-4 col-md-6 mb-4">
-    //         <a href="details.html?id=${product.id}" title="Shop ${product.title}">    
-    //             <div class="card rounded shadow-lg border-0 h-100 zoom-button">
-    //                 <div class="card-body text-center p-0">
-    //                     <img src="${strapiUrl}${product.image.url}"
-    //                         alt="${product.title}" class="img-fluid w-100 d-block mx-auto mb-3">
-    //                     <h3 class="fs-5 nav-item">
-    //                         <a href="../details.html?id=${product.id}" title="Shop ${product.title}" class="nav-link text-dark">${product.title}</a>
-    //                     </h3>
-    //                     <p>$${product.price}</p>
-    //                 </div>
-    //             </div>
-    //         </a>    
-    //     </div>
-    //     `
-    // }
-
+    if(counter === 8){
+        thirdSlider = `
+        <div class="carousel-item">
+            <div class="row">${allSlider}</div>
+        </div>
+        `; 
+        allSlider = "";     
+    }
     counter ++;
+    
 
+    if(counter > 8) return;
 
 });
+
+carrouselContainer.innerHTML = firstSlider + secondSlider + thirdSlider;
 
 return ;
     
