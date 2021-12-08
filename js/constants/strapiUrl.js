1fdc653fe39548937e63ea0e8ac0a6ba6c5b15c0
NetiusSP <fabiomoraisnetosp@hotmail.com>
2021-12-08 16:20:04 +0100

    Added localhost urls for development local

--- js/constants/strapiUrl.js
+++ js/constants/strapiUrl.js
@@ -1,3 +1,9 @@
-export const heroStrapiUrl = "https://safe-retreat-07204.herokuapp.com/home/";
-export const productStrapiUrl = "https://safe-retreat-07204.herokuapp.com/products/";
-export const strapiUrl = "https://safe-retreat-07204.herokuapp.com";
+// Development - Localhost
+export const heroStrapiUrl = "http://localhost:1337/home/";
+export const productStrapiUrl = "http://localhost:1337/products/";
+export const strapiUrl = "http://localhost:1337";
+
+// Production - Heroku 
+// export const heroStrapiUrl = "https://safe-retreat-07204.herokuapp.com/home/";
+// export const productStrapiUrl = "https://safe-retreat-07204.herokuapp.com/products/";
+// export const strapiUrl = "https://safe-retreat-07204.herokuapp.com";
