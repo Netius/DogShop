5b6d6f02e2a4dab8aee68b03c97e69f310e8f986
NetiusSP <fabiomoraisnetosp@hotmail.com>
2021-12-08 15:40:23 +0100

    Using heroku adress to fetch products

--- js/constants/strapiUrl.js
+++ js/constants/strapiUrl.js
@@ -1,3 +1,3 @@
-export const heroStrapiUrl = "http://localhost:1337/home/";
-export const productStrapiUrl = "http://localhost:1337/products/";
-export const strapiUrl = "http://localhost:1337";
+export const heroStrapiUrl = "https://safe-retreat-07204.herokuapp.com/home/";
+export const productStrapiUrl = "https://safe-retreat-07204.herokuapp.com/products/";
+export const strapiUrl = "https://safe-retreat-07204.herokuapp.com";
