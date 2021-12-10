f0f6aa69423409dc1caa75d8d4bcbe8c88361739
NetiusSP <fabiomoraisnetosp@hotmail.com>
2021-12-10 11:38:11 +0100

    Fixed login and logout as admin

--- js/components/login/loggedIn.js
+++ js/components/login/loggedIn.js
@@ -1,7 +1,28 @@
+import { clearStorage, getUser } from "../../utils/storage.js";
+
+const user = getUser(); 
+
 export default function logggedIn(token){
-    const loginLink = document.getElementById("login-link");
+    const loginLink = document.getElementById("login-container");
 
     if(token){
-        loginLink.innerHTML = `<span>YES</span>`
+        loginLink.innerHTML = `
+        <a class="nav-link text-reset navigation__admin zoom" title="Logout" 
+            href="login.html" id="logout-link" role="link">
+            ${user.username} <i class="fas fa-sign-in-alt fa-lg text-danger"></i>
+        </a>`
+
+        const logoutBtn = document.getElementById("logout-link");
+        logoutBtn.addEventListener("click" , clearStorage);
+
+        return loginLink;
     }
+
+    loginLink.innerHTML = `
+    <a class="nav-link text-reset navigation__admin zoom" title="Login to admin page" 
+        href="login.html" id="login-link" role="link">
+        Login <i class="fas fa-sign-in-alt fa-lg"></i>
+    </a>
+    `
+    return loginLink;
 }
