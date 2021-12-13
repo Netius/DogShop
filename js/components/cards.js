ba4b799acaaf2eb0a5180de9451df57c6d5f85d8
NetiusSP <fabiomoraisnetosp@hotmail.com>
2021-12-13 15:55:44 +0100

    Fixed no image in cards

--- js/components/cards.js
+++ js/components/cards.js
@@ -1,4 +1,4 @@
-import { strapiUrl } from "../constants/strapiUrl.js"
+import hasProductImage from "../utils/hasProductImage.js";
 
 export default function cardsProducts (products){
     const cardsContainer = document.getElementById("cards-container");
@@ -7,13 +7,14 @@ export default function cardsProducts (products){
     products.forEach(product => {      
         if(!product.featured) return; 
         if(counter === 3) return;
+        const productImageUrl = hasProductImage(product);
         
         cardsContainer.innerHTML += `
         <div class="col-lg-4 col-md-6 mb-4">
             <a href="details.html?id=${product.id}" title="Shop ${product.title}">    
                 <div class="card rounded shadow-lg border-0 h-100 zoom-button">
                     <div class="card-body text-center p-0">
-                        <img src="${strapiUrl}${product.image?.url}"
+                        <img src="${productImageUrl}"
                             alt="${product.title}" class="img-fluid w-100 d-block mx-auto mb-3">
                         <h3 class="fs-5 nav-item">
                             <a href="../details.html?id=${product.id}" title="Shop ${product.title}" class="nav-link text-dark">${product.title}</a>
