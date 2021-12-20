80319a0bb6313602c952f00b19578bbcd45b2609
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2021-12-20 14:32:39 +0100

    Exported to other components

--- js/components/details/productDetails.js
+++ js/components/details/productDetails.js
@@ -1,5 +1,5 @@
-import { strapiUrl } from "../../constants/strapiUrl.js";
 import hasProductImage from "../../utils/hasProductImage.js";
+
 
 export default function productDetails(product){
     const detailsContainer = document.getElementById("details-container");
@@ -7,14 +7,14 @@ export default function productDetails(product){
     
     detailsContainer.innerHTML = `
                 <div class="row py-5">
-                    <div class="col-lg-6 col-sm-9">
+                    <div class="col-lg-6 col-sm-12">
                         <img src="${productImageUrl}" class="w-100 img-responsive">
                     </div>
                     <div class="col-lg-6 col-sm-12">
                         <h2 class="display-6 mt-3">${product.title}</h2>
                         <p>${product.description}</p>
                         <p class="mt-5 mb-3 h3">$${product.price}</p>
-                        <button class="zoom-button btn btn-my-primary mr-1" data-toggle="tooltip" title="Add to cart" data-original-title="Add to cart">
+                        <button id="cart-button" class="zoom-button btn btn-my-primary mr-1" data-toggle="tooltip" title="Add to cart" data-id="${product.id}">
                             <i class="fa fa-shopping-cart"></i>
                         </button>
                         <button class="ms-1 zoom-button fs-6 btn btn-my-info btn-feature" 
@@ -30,5 +30,8 @@ export default function productDetails(product){
                     </div>
                 </div>
     `
+
+    const cartBtn = document.getElementById("cart-button");
+    cartBtn.addEventListener("click" , addToCart)
     return detailsContainer;
 }
--- js/utils/storage.js
+++ js/utils/storage.js
@@ -23,11 +23,11 @@ export function getUser() {
     return null;
 }
 
-function saveToStorage(key, value) {
+export function saveToStorage(key, value) {
     localStorage.setItem(key, JSON.stringify(value));
 }
 
-function getFromStorage(key) {
+export function getFromStorage(key) {
     const value = localStorage.getItem(key);
 
     if (!value) {
