9fff9d4140b30ab1954d0e4f7f0b6c6cb392b629
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2021-12-30 21:26:04 +0100

    Checks if images is in Strapi or on Cloudinary

--- js/utils/hasProductImage.js
+++ js/utils/hasProductImage.js
@@ -3,8 +3,15 @@ import { strapiUrl } from "../constants/strapiUrl.js";
 export default function hasProductImage(product){
     let placeholder = "https://via.placeholder.com/600x400/D9C2AD8C/808080 C/O https://placeholder.com/"
     
-    if(product.image?.url !== undefined) {
-        return strapiUrl + product.image.url;
+    if(product.image) {
+        // Checking if images is in Strapi or on Cloudinary
+        if(product.image.provider === "local")
+            return strapiUrl + product.image.url;
+        else{
+            return product.image.url;
+        }
     }
+
+
     return placeholder;
 }
